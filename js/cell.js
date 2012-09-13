710f49b0d3ff309e7aa840f109a4e63c167694e9
Chris Wilson <cwilso@gmail.com>
2012-09-12 17:13:31 -0700

    Updated to make selection of MIDI interfaces work (and prefer Launchpad)

--- js/cell.js
+++ js/cell.js
@@ -7,10 +7,6 @@ var frame1 = null,
 var numRows = 8,
 	numCols = 8;
 
-var midi = null;
-var midiOut = null;
-var midiIn = null;
-
 window.addEventListener('keydown', function() { tick();
 } );
 
@@ -46,22 +42,91 @@ window.addEventListener('load', function() {
 	}
 	currentFrame = frame1;
 	backFrame = frame2;
-	navigator.getMIDIAccess( gotMIDI, null );
+	navigator.getMIDIAccess( onMIDIInit, null );
 } );
 
-function gotMIDI( midiAccess ) {
-	midi = midiAccess;
-	var ins = midiAccess.enumerateInputs();
-	midiIn = midiAccess.getInput( ins[0] );
-	midiIn.onmessage = midiProc;
+var selectMIDIIn = null;
+var selectMIDIOut = null;
+var midiAccess = null;
+var midiIn = null;
+var midiOut = null;
 
-	var outs = midiAccess.enumerateOutputs();
-	midiOut = midiAccess.getOutput( outs[0] );
+function changeMIDIIn( ev ) {
+  var list=midiAccess.enumerateInputs();
+  var selectedIndex = ev.target.selectedIndex;
 
+  if (list.length >= selectedIndex) {
+    midiIn = midiAccess.getInput( list[selectedIndex] );
+    midiIn.onmessage = midiMessageReceived;
+  }
+}
+
+function changeMIDIOut( ev ) {
+  var list=midiAccess.enumerateOutputs();
+  var selectedIndex = ev.target.selectedIndex;
+
+  if (list.length >= selectedIndex) {
+    midiOut = midiAccess.getOutput( list[selectedIndex] );
 	midiOut.sendMessage( 0xB0,0x00,0x00 ); // Reset Launchpad
 	midiOut.sendMessage( 0xB0,0x00,0x01 ); // Select XY mode
 	drawFullBoardToMIDI();
+  }
 }
+
+function onMIDIInit( midi ) {
+  var preferredIndex = 0;
+  midiAccess = midi;
+  selectMIDIIn=document.getElementById("midiIn");
+  selectMIDIOut=document.getElementById("midiOut");
+
+  var list=midiAccess.enumerateInputs();
+
+  // clear the MIDI input select
+  selectMIDIIn.options.length = 0;
+
+  for (var i=0; i<list.length; i++)
+    if (list[i].name.toString().indexOf("Launchpad") != -1)
+      preferredIndex = i;
+
+  if (list.length) {
+    for (var i=0; i<list.length; i++)
+      selectMIDIIn.options[i]=new Option(list[i].name,list[i].fingerprint,i==preferredIndex,i==preferredIndex);
+
+    midiIn = midiAccess.getInput( list[preferredIndex] );
+    midiIn.onmessage = midiProc;
+
+    selectMIDIIn.onchange = changeMIDIIn;
+  }
+
+  // clear the MIDI output select
+  selectMIDIOut.options.length = 0;
+  preferredIndex = 0;
+  list=midi.enumerateOutputs();
+
+  for (var i=0; i<list.length; i++)
+    if (list[i].name.toString().indexOf("Launchpad") != -1)
+      preferredIndex = i;
+
+  if (list.length) {
+    for (var i=0; i<list.length; i++)
+      selectMIDIOut.options[i]=new Option(list[i].name,list[i].fingerprint,i==preferredIndex,i==preferredIndex);
+
+    midiOut = midiAccess.getOutput( list[preferredIndex] );
+    selectMIDIOut.onchange = changeMIDIOut;
+  }
+
+  if (midiOut) {  
+	midiOut.sendMessage( 0xB0,0x00,0x00 ); // Reset Launchpad
+	midiOut.sendMessage( 0xB0,0x00,0x01 ); // Select XY mode
+	drawFullBoardToMIDI();
+  }
+}
+
+
+
+
+
+
 
 function flipHandler(e) {
 	flip( e.target );
